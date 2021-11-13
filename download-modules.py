93effdb2a1562480cb2fadcaeca45aeb8998567d
Nikolay Gniteev <godhart@gmail.com>
2021-11-13 22:02:51 +0300

    Initial version

---
+++ download-modules.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import shutil
+import sys
+
+if __name__ == "__main__":
+    force = "--force" in sys.argv
+    force = True
+
+    with open("requirements.txt", "r") as f:
+        packages = f.readlines()
+
+    for root, dirs, files in os.walk("."):
+        for d in dirs:
+            if len(d.split("-")) != 2:
+                continue
+            for p in packages:
+                package = p.strip()
+                if package == "" or package[0] == "#":
+                    continue
+                target_py, target_os = d.split("-")
+                target_path = f"./{target_py}-{target_os}/{package}"
+                if os.path.exists(target_path):
+                    if force:
+                        shutil.rmtree(target_path)
+                    else:
+                        continue
+                os.makedirs(target_path)
+                result = subprocess.run(["pip-download", "-py", target_py, "-p", target_os, package], cwd=target_path,
+                                        capture_output=True)
+                if result.returncode < 0:
+                    continue
+                # lines = result.stdout.decode().split("\n")
+                with open(f"{target_path}/install.bat", "w") as f:
+                    f.write(f"python -m pip install {package} --no-index --find-link .")
+        break
